611945ddbffd03115f78851b88942d7c551c82ce
Yunis <yunismohamed1@gmail.com>
2019-01-10 11:47:04 +0300

    [feature #163055885] Add imports from meetups_model

--- app/api/v1/views/meetups_view.py
+++ app/api/v1/views/meetups_view.py
@@ -3,6 +3,8 @@ Responses to requests regarding meetups are set up here"""
 
 from flask import Blueprint, request, jsonify
 
+from app.api.v1.models.meetups_model import Meetup
+
 v1 = Blueprint('v1', __name__, url_prefix='/api/v1')
 
 @v1.route("/meetups", methods=['POST'])
@@ -33,6 +35,16 @@ def create_meetup():
     if not tags:
         return jsonify({'status':400, 'error':'Missing tags field'}), 400
 
-        
+    new_meetup = Meetup(
+        topic=topic,
+        happeningOn=happeningOn,
+        location=location,
+        images=images,
+        tags=tags
+    )
+
+    new_meetup.add_meetup_record()
+
+    
 
 
