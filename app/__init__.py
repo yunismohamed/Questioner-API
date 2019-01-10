cfd3f76df263fa625a4881042a6f283feb3833ee
Yunis <yunismohamed1@gmail.com>
2019-01-10 13:41:08 +0300

    [feature #163055885] Register v1 blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -12,5 +12,6 @@ def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
+    app.register_blueprint(v1)
 
     return app 
