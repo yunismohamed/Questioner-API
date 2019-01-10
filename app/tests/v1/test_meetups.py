8fdb16cc5a44056e2719611c69f03bd33dd9d26f
Yunis <yunismohamed1@gmail.com>
2019-01-11 01:31:23 +0300

    [feature #163055885] Adjusts configurations for tests to run smoothly

--- app/tests/v1/test_meetups.py
+++ app/tests/v1/test_meetups.py
@@ -3,11 +3,8 @@ Tests for the meetups are defined here"""
 
 import unittest
 import json
-from datetime import datetime
 
 from app import create_app
-
-createdOn = datetime.now()
 
 
 class BaseTest(unittest.TestCase):
@@ -16,12 +13,9 @@ class BaseTest(unittest.TestCase):
     The base test that occurs before any other test"""
 
     def setUp(self):
-        self.app = create_app("testing")
-        self.client = self.app.test_client        
-
+        self.app = create_app()
+        self.client = self.app.test_client()
         self.meetup = {
-            "id": 1,
-            "createdOn": createdOn,
             "topic": "Data Science Meetup, Nairobi",
             "location": "Nairobi",
             "images": ["datascience.png", "python.png"],
@@ -36,20 +30,18 @@ class TestMeetups(BaseTest):
 
     def test_create_meetup_record(self):
         """Function to test creation of a meetup record"""
-        url = '/api/v1/meetup/create'
+        url = '/api/v1/meetups'
 
-        response= self.client.post(url, data=json.dumps(self.meetup), content_type="application/json")
+        response = self.client.post(url, data=json.dumps(self.meetup),
+                                    content_type="application/json")
         result = json.loads(response.data.decode('UTF-8'))
 
         self.assertEqual(response.status_code, 201)
         self.assertEqual(result["status"], 201)
         self.assertEqual(result["data"], [
             {
-                "id": 1,
-                "createdOn": createdOn,
                 "topic": "Data Science Meetup, Nairobi",
                 "location": "Nairobi",
-                "images": ["datascience.png", "python.png"],
                 "happeningOn": "Tuesday 1 2015",
                 "tags": ["data", "python"]
             }
--- run.py
+++ run.py
@@ -5,8 +5,7 @@ import os
 
 from app import create_app
 
-config_name = os.getenv('APP_SETTINGS')  # config_name = "development"
-app = create_app(config_name)
+app = create_app()
 
 if __name__ == '__main__':
     app.run()
