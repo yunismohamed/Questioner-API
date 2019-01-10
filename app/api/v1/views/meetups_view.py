807cc94df644f80c8c08cdef0d7342350dbfd6f3
Yunis <yunismohamed1@gmail.com>
2019-01-10 10:59:55 +0300

    [feature #163055885] Add the /meetups route

--- app/api/v1/views/meetups_view.py
+++ app/api/v1/views/meetups_view.py
@@ -1,7 +1,9 @@
 """
 Responses to requests regarding meetups are set up here"""
 
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
 
 v1 = Blueprint('v1', __name__, url_prefix='/api/v1')
+
+@v1.route("/meetups", methods=['POST'])  
 
