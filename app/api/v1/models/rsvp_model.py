20fada7a49deeab9155a6de434e5865a9e30767a
Yunis <yunismohamed1@gmail.com>
2019-01-15 21:26:12 +0300

    [feature #163067301] Add model function definition to set rsvp and save to DB

--- app/api/v1/models/rsvp_model.py
+++ app/api/v1/models/rsvp_model.py
@@ -15,5 +15,15 @@ class Rsvp:
         """
         self.db = RSVP
 
-    def set_rsvp(self, *args):
-        pass
+    def set_rsvp(self, meetup_id, user_id, response):
+        """
+        Sets the rsvp status for a meetup and adds it to the DB
+        """
+        rsvp = {
+            "id": len(self.db) + 1,
+            "meetup": meetup_id,
+            "user": user_id,
+            "response": response
+        }
+
+        RSVP.append(rsvp)
