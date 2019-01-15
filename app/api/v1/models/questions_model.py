98ca04e836e8430bc0151d5edf71de46d4042523
Yunis <yunismohamed1@gmail.com>
2019-01-15 07:27:35 +0300

    [feature #163067059]Add the downvote model method definitaion

--- app/api/v1/models/questions_model.py
+++ app/api/v1/models/questions_model.py
@@ -49,5 +49,17 @@ class Questions:
             "votes": question[0]["votes"]
         }
 
-    def downvote_question(self):
-        pass
+    def downvote_question(self, id):
+        question = [question for question in self.db if question["id"] == id]
+
+        if not question:
+            return False
+
+        question[0]["votes"] -= 1
+
+        return {
+            "meetup": question[0]["meetup"],
+            "title": question[0]["title"],
+            "body": question[0]["body"],
+            "votes": question[0]["votes"]
+        }
