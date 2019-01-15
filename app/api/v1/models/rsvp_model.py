b3c58d6ac0786920e6d08ce5eb3daec572dba8a2
Yunis <yunismohamed1@gmail.com>
2019-01-15 19:23:18 +0300

    [feature #163067301] Add the rsvp class

--- app/api/v1/models/rsvp_model.py
+++ app/api/v1/models/rsvp_model.py
@@ -4,3 +4,13 @@ The rsvp models are defined here"""
 RSVP = []  # DB of all rsvps
 
 
+class Rsvp:
+
+    """
+    Contains models for the rsvp"""
+
+    def __init__(self):
+        """
+        Initializes the rsvp class
+        """
+        self.db = RSVP
