57055b612339b95a5870ee63cd7b36401b567172
Yunis <yunismohamed1@gmail.com>
2019-01-16 21:37:26 +0300

    [finishes #163067889] Register the auth blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -7,11 +7,13 @@ from flask import Flask
 
 from .api.v1.views.meetups_view import v1
 from .api.v1.views.questions_view import questions_v1
+from .api.v1.views.users_view import auth
 
 
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(v1)
     app.register_blueprint(questions_v1)
+    app.register_blueprint(auth)
 
     return app
