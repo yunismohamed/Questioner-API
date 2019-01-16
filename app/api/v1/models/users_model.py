69329ff8da6a2650e9227988a20b57f3dadc81d9
Yunis <yunismohamed1@gmail.com>
2019-01-16 21:32:39 +0300

    [feature #163067889] Adds users class

--- app/api/v1/models/users_model.py
+++ app/api/v1/models/users_model.py
@@ -4,3 +4,28 @@ The users models are defined here"""
 ALL_USERS = []  # DB of all users
 
 
+class Users:
+
+    """
+    Contains models for the users"""
+
+    def __init__(self):
+        """
+        Initializes the rsvp class
+        """
+        self.db = ALL_USERS
+
+    def add_user(self, firstname, lastname, email, username, password, role):
+        """ Sets the details of the new user and adds the user to DB """
+
+        new_user = {
+            "id": len(self.db) + 1,
+            "firstname": firstname,
+            "lastname": lastname,
+            "email": email,
+            "username": username,
+            "password": password,
+            "role": role
+        }
+
+        ALL_USERS.append(new_user)
